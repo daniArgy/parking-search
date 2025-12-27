b71fb3797f8344b2bf6ee5f8c9bf177d3fd868f0
daniel <dgonzalezargibay@gmail.com>
2025-12-28 00:18:22 +0100

    Fix Vercel deploy

---
+++ api/index.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Vercel Bridge
+ * This file acts as a bridge to the real backend entry point
+ * to comply with Vercel's standard function location.
+ */
+
+require __DIR__ . '/../backend/public/index.php';
